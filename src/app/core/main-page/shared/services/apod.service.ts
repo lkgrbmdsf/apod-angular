4dbcf1d6be13549045913bc3246a964089868060
stepa-kuropatkin <igortrotsuk@gmail.com>
2021-12-24 19:13:30 +0300

    feat: fakedb upd

--- src/app/core/main-page/shared/services/apod.service.ts
+++ src/app/core/main-page/shared/services/apod.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApodModel } from '../models/apod-model';
-import { URL, DEMO_URL } from '../constants/const-values';
+import { URL } from '../constants/const-values';
 import { map } from 'rxjs/operators';
+import { DB_URL } from 'src/app/core/login-page/shared/constants/constant-urls';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class ApodService {
   constructor(private httpClient: HttpClient) {}
 
   fetchFakeResults(startDate?: string, endDate?: string): Observable<ApodModel[]> {
-    const response = this.httpClient.get<ApodModel[]>(DEMO_URL);
+    const response = this.httpClient.get<ApodModel[]>(DB_URL + 'fakeDB');
     return response;
   }
 
