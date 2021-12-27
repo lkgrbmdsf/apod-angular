cd62d2a024d23b1f98a08693ed6059eeb241760d
stepa-kuropatkin <igortrotsuk@gmail.com>
2021-12-27 12:47:54 +0300

    for test

--- src/app/core/main-page/shared/constants/const-values.ts
+++
@@ -1,8 +0,0 @@
-export const URL =
-  'https://api.nasa.gov/planetary/apod?api_key=P3tahVnbYbyc1EsOgSXVCXhgzEkn7lAlPcOT5181&';
-
-export const DEMO_URL = '../../../../assets/temp/data.json';
-
-export const TODAYS_DATE = new Date().toISOString().substring(0, 10);
-
-export const FIRST_APOD_DATE = '1995-06-16';
